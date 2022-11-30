8d2615faf3834892a82128c29c00eab980227012
ema_ryu-ylab-pc <ee207008@meiji.ac.jp>
2022-11-30 21:34:18 +0900

    変更を保存

--- src/drawTest.ts
+++ src/drawTest.ts
@@ -12,12 +12,16 @@ const sketch = (p: p5) => {
     p.createCanvas(p.windowWidth, p.windowHeight); //キャンバスの作成
     p.background("#000000"); // 背景色を設定(黒)
     p.noStroke(); // 線なし（塗りつぶしのみ）に設定
+    p.angleMode(p.DEGREES); // 回転の単位を弧度から角度に変更
   };
 
   /** フレームごとの描画処理 */
   p.draw = () => {
-    p.fill(p.lerpColor(color2, color1, color1amount)); // 塗り色の設定
-    p.ellipse(p.width / 2, p.height / 2, 500, 500); // 楕円の描画
+    p.fill(p.lerpColor(color2, color1, color1amount)); // 塗り色を設定
+    p.translate(p.width / 2, p.height / 2); // 画面中央を原点に
+    p.rotate(p.frameCount * 45);  // フレーム数分（1フレームあたり90度）回転させる
+    p.ellipse(p.frameCount / 2, 0, p.frameCount, p.frameCount / 3); // 楕円を描く
+    color1amount *= 0.995; // 「描画色1の強さ」を少し弱くする
   };
 }
 
