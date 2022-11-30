d3e5b9088d69c6a12525846464d739e5eab4a845
RyunosukeEma <ee207008@meiji.ac.jp>
2022-12-01 00:44:19 +0900

    インデントの修正

--- src/drawLoading.js
+++ src/drawLoading.js
@@ -3,7 +3,7 @@ var CENTER_WINDOW_WIDTH = WIDTH_SIZE / 2;
 var CENTER_WINDOW_HEIGHT = HEIGHT_SIZE / 2;
 var LOAD_TIME_RESET = 9
 var NOW_LOADING = new Array("N", "O", "W", "L", "O", "A", "D", "I", "N", "G");
- 
+
 class Mark {
   constructor(x, y) {
     this.deleteCount = 60;
@@ -12,38 +12,38 @@ class Mark {
     this.w = 50;
     this.fadeout = 1.0;
   }
-  addDeleteCount(){
+  addDeleteCount() {
     this.deleteCount--;
     this.fadeout -= 1 / 60;
     this.w += 10;
   }
 }
- 
+
 var marks = new Array();
- 
+
 function setup() {
-  translate(CENTER_WINDOW_WIDTH , CENTER_WINDOW_HEIGHT);
+  translate(CENTER_WINDOW_WIDTH, CENTER_WINDOW_HEIGHT);
   createCanvas(WIDTH_SIZE, HEIGHT_SIZE);
-  $("body").css("margin","0");
+  $("body").css("margin", "0");
   displayCount = 0;
   loadCount = 0;
 }
- 
+
 function draw() {
   translate(CENTER_WINDOW_WIDTH, CENTER_WINDOW_HEIGHT);
   background(50);
   loading(loadCount)
   displayMark();
   displayCount++;
-  if(displayCount % 10 == 0) {
-    if(loadCount > LOAD_TIME_RESET){
+  if (displayCount % 10 == 0) {
+    if (loadCount > LOAD_TIME_RESET) {
       loadCount = 0;
     } else {
       loadCount++;
     }
   }
 }
- 
+
 function loading(loadCount) {
   textAlign(CENTER);
   textSize(30);
@@ -51,22 +51,22 @@ function loading(loadCount) {
   fill(255);
   var display_width = 200;
   var display_height = 290;
- 
-  for(var i = 0; i < 10; i++) {
-    if(i == ((loadCount) % 10)) {
-      text(NOW_LOADING[i], display_width + (i*30), display_height - 30)
+
+  for (var i = 0; i < 10; i++) {
+    if (i == ((loadCount) % 10)) {
+      text(NOW_LOADING[i], display_width + (i * 30), display_height - 30)
     } else {
-      text(NOW_LOADING[i], display_width + (i*30), display_height)
+      text(NOW_LOADING[i], display_width + (i * 30), display_height)
     }
   }
 }
- 
-function displayMark(){
+
+function displayMark() {
   translate(-CENTER_WINDOW_WIDTH, -CENTER_WINDOW_HEIGHT);
-  for(var i = 0; marks[i] !== undefined ; i++) {
+  for (var i = 0; marks[i] !== undefined; i++) {
     mark = marks[i];
     if (mark.deleteCount == 0) {
-      marks.splice(i,1);
+      marks.splice(i, 1);
       continue;
     }
     fill('rgba(255,255,255,' + mark.fadeout + ')');
@@ -75,8 +75,8 @@ function displayMark(){
     mark.addDeleteCount();
   }
 }
- 
-function mousePressed(){
+
+function mousePressed() {
   translate(-CENTER_WINDOW_WIDTH, -CENTER_WINDOW_HEIGHT);
   marks.push(new Mark(mouseX, mouseY));
 }
