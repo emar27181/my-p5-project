bb25d69ab5978884faba67ee69272aa0f2cd6273
ema_ryu-home-desktop-pc <ee207008@meiji.ac.jp>
2022-12-03 03:42:34 +0900

    switch文の追加(バグ有)

--- src/drawLine.ts
+++ src/drawLine.ts
@@ -1,3 +1,4 @@
+// 素数フレーム進んだら進路を90°右回転するプログラム
 import p5 from "p5";
 
 const sketch = (p: p5) => {
@@ -6,7 +7,7 @@ const sketch = (p: p5) => {
   const color1 = p.color("#ffffff"); //描画色1(白)
   const color2 = p.color("#000000"); //描画色2(黒)
   let color1amount = 1; // 描画色1の強さ
-  var i = 0;
+  let i = 0, x = 0, y = 0;
 
   /* 初期化処理 */
   p.setup = () => {
@@ -19,10 +20,33 @@ const sketch = (p: p5) => {
   p.draw = () => {
     p.fill(p.lerpColor(color2, color1, color1amount)); // 塗り色の設定
 
+    console.log("i:     " + i);
+    console.log("i % 4: " + i % 4);
+
     // 点の描画
-    // p.point(i, i); //何故かpoint()が上手く動作していない？？
-    p.ellipse(i,i, 3, 3); 
-    i++; 
+    p.ellipse(p.width / 2 + x, p.height / 2 + y, 3, 3);
+
+    i++;
+
+    switch (i % 4) {
+      case 0:
+        x++;
+        y++;
+      case 1:
+        x--;
+        y++;
+      case 2:
+        x--;
+        y--;
+      case 3:
+        x++;
+        y--;
+      default:
+        x -= 0.5;
+        y -= 0.5;
+      //console.log("this is default");
+    }
+
 
   };
 }
