7e6371a69665e10051d34130c9d12946c73cd774
ema_ryu-home-desktop-pc <ee207008@meiji.ac.jp>
2022-12-03 19:10:11 +0900

    素数の出現で右に90°進路を変更する描画の仮実装

--- src/drawLine.ts
+++ src/drawLine.ts
@@ -7,7 +7,7 @@ const sketch = (p: p5) => {
   const color1 = p.color("#ffffff"); //描画色1(白)
   const color2 = p.color("#000000"); //描画色2(黒)
   let color1amount = 1; // 描画色1の強さ
-  let i = 0, x = 0, y = 0;
+  let i = 0, x = 0, y = 0, direction = 0;
   let isPrime = false;
 
   /* 初期化処理 */
@@ -22,8 +22,8 @@ const sketch = (p: p5) => {
     p.fill(p.lerpColor(color2, color1, color1amount)); // 塗り色の設定
 
     // 点の描画
-    // p.ellipse(p.width / 2 + x, p.height / 2 + y, 3, 3);
-    p.ellipse(x, y, 3, 3);
+    const div = 4;
+    p.ellipse(p.width / 2 + x / div, p.height / 2 + y / div, 1, 1);
 
     //i: 割られる数
     //j: 割る数
@@ -44,22 +44,28 @@ const sketch = (p: p5) => {
       }
     }
 
-    // 確認用出力
     if (isPrime === true) {
+      direction++;
+      // 確認用出力
       console.log("i: " + i);
+      console.log("direction: " + direction);
+      console.log("direction % 4: " + direction % 4);
     }
 
     //次に打つ点の制御
-    switch (isPrime) {
-      case true:
+    switch (direction % 4) {
+      case 0:
         x++;
+        break;
+      case 1:
         y++;
         break;
-
-      case false:
-        x++;
+      case 2:
+        x--;
+        break;
+      case 3:
         y--;
-
+        break;
       default:
         console.log("this is default");
         break;
