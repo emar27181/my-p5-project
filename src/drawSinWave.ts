e55a92d02e1872466c2e7fad67e0d773977659e5
RyunosukeEma <ee207008@meiji.ac.jp>
2022-12-07 14:07:05 +0900

    drawSinWaveのファイルの用意(中身はテンプレート)

---
+++ src/drawSinWave.ts
@@ -0,0 +1,24 @@
+import p5 from "p5";
+
+const sketch = (p: p5) => {
+
+    //色の指定
+    const color1 = p.color("#ffffff"); //描画色1(白)
+    const color2 = p.color("#000000"); //描画色2(黒)
+    let color1amount = 1; // 描画色1の強さ
+
+    /** 初期化処理 */
+    p.setup = () => {
+        p.createCanvas(p.windowWidth, p.windowHeight); //キャンバスの作成
+        p.background("#000000"); // 背景色を設定(黒)
+        p.noStroke(); // 線なし（塗りつぶしのみ）に設定
+    };
+
+    /** フレームごとの描画処理 */
+    p.draw = () => {
+        p.fill(p.lerpColor(color2, color1, color1amount)); // 塗り色の設定
+        p.ellipse(p.width / 2, p.height / 2, 10, 10); // 楕円の描画(中央)
+    };
+}
+
+new p5(sketch);
