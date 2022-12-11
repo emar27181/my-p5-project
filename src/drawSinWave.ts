511282a13f630281542673fe652bf4f0d3fb8578
ema_ryu-home-desktop-pc <ee207008@meiji.ac.jp>
2022-12-11 20:20:51 +0900

    進路の変更と振幅の変更の実装

--- src/drawSinWave.ts
+++ src/drawSinWave.ts
@@ -8,22 +8,54 @@ const sketch = (p: p5) => {
     let color1amount = 1; // 描画色1の強さ
 
     /** 初期化処理 */
+    //p.windowWidth: 464
+    //p.windowHeight: 464
+    //p.width: 464
+    //p.height: 648
     p.setup = () => {
         p.createCanvas(p.windowWidth, p.windowHeight); //キャンバスの作成
         p.background("#000000"); // 背景色を設定(黒)
         p.noStroke(); // 線なし（塗りつぶしのみ）に設定
     };
 
-    
+
+    //direction: 進路(0-> 右進行, 1 -> 左進行)
+    //amplitude: 振幅
     let x = 0;
+    let direction = 0;
+    let amplitude = 50;
+
     /** フレームごとの描画処理 */
     p.draw = () => {
         p.fill(p.lerpColor(color2, color1, color1amount)); // 塗り色の設定
 
-        x++;
-            let radian = p.TWO_PI / 150 * x; 
-            let y = 100 * p.sin(radian);
-            p.ellipse(x, -y + p.height / 2, 1, 1); // 楕円の描画(中央)
+        //進路の決定
+        //右端の場合
+        if (x == p.width) {
+            direction = 1;
+            amplitude += 50;
+        }
+        //左端の場合
+        if (x == 0) {
+            direction = 0;
+            amplitude += 50;
+        }
+
+        //打点
+        //右進行の場合
+        if (direction == 0) {
+            x++;
+            let radian = p.TWO_PI / 150 * x;
+            let y = amplitude * p.sin(radian);
+            p.ellipse(x, -y + p.height / 2, 1, 1); // 点の描画
+        }
+        //左進行の場合
+        else if (direction == 1) {
+            x--;
+            let radian = p.TWO_PI / 150 * x;
+            let y = amplitude * p.sin(radian);
+            p.ellipse(x, -y + p.height / 2, 1, 1); // 点の描画
+        }
 
     };
 }
