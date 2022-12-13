7e89493ecaf7f4041ae0432a8536c25f8d615fe5
ema_ryu-home-desktop-pc <ee207008@meiji.ac.jp>
2022-12-14 03:05:19 +0900

    drawRipplesの最低限の実装

--- src/drawRipples.ts
+++ src/drawRipples.ts
@@ -3,6 +3,8 @@ import p5 from "p5";
 const sketch = (p: p5) => {
 
   //色の指定
+
+
   const color1 = p.color("#ffffff"); //描画色1(白)
   const color2 = p.color("#000000"); //描画色2(黒)
   let color1amount = 1; // 描画色1の強さ
@@ -15,16 +17,32 @@ const sketch = (p: p5) => {
     //p.width: 464
     //p.height: 648
     p.createCanvas(p.windowWidth, p.windowHeight); //キャンバスの作成
-    p.background("#000000"); // 背景色を設定(黒)
+    p.background("#000000"); // 背景色を設定(白)
     p.noStroke(); // 線なし（塗りつぶしのみ）に設定
   };
 
+  let x = 0, y = 0, r = 0;
+
   /** フレームごとの描画処理 */
   p.draw = () => {
-    p.fill(p.lerpColor(color2, color1, color1amount)); // 塗り色の設定
-    p.ellipse(0, 0, 100, 100); // 楕円の描画(左上)
-    p.ellipse(p.width / 2, p.height / 2, 100, 100); // 楕円の描画(中央)
-    p.ellipse(p.width, p.height, 100, 100); // 楕円の描画(右下)
+
+    // p.fill("999999");
+    // p.rect(0, p.height / 2 , p.width, p.height); 
+    // p.text("x: " + p.str(p.mouseX) + ", y:" + p.str(p.mouseY), 0, p.height - 10); //表示すると色が張り付いてしまう
+
+    r++;
+
+    //p.fill(p.lerpColor(color2, color1, color1amount)); // 塗り色の設定
+    //p.ellipse(x, y, r, r);
+    p.fill("ffffff");
+    p.ellipse(x, y, r-3, r-3);
+
+    if (p.mouseIsPressed) {
+      p.ellipse(p.mouseX, p.mouseY, 3);
+      x = p.mouseX;
+      y = p.mouseY;
+      r = 0;
+    }
   };
 }
 
