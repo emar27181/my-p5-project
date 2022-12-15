78fa21f74d0dfbb4804eb9887539632d72b36891
ema_ryu-ylab-pc <ee207008@meiji.ac.jp>
2022-12-15 21:00:57 +0900

    ランダムカラーを描画するブラシの追加

--- src/drawUniqueBrush.ts
+++ src/drawUniqueBrush.ts
@@ -22,27 +22,34 @@ const sketch = (p: p5) => {
   };
 
   let x = 0, y = 0, r = 0;
+  const blushSize = 20;
 
   /** フレームごとの描画処理 */
   p.draw = () => {
-
-    // p.fill("999999");
-    // p.rect(0, p.height / 2 , p.width, p.height); 
-    // p.text("x: " + p.str(p.mouseX) + ", y:" + p.str(p.mouseY), 0, p.height - 10); //表示すると色が張り付いてしまう
-
-    r++;
-
-    //p.fill(p.lerpColor(color2, color1, color1amount)); // 塗り色の設定
-    //p.ellipse(x, y, r, r);
     p.fill("ffffff");
-    p.ellipse(x, y, r-3, r-3);
+    // p.ellipse(x, y, r-3, r-3);
+
+    //ランダムカラーの生成と宣言
+    var color = (Math.random() * 0xFFFFFF | 0).toString(16);
+    var randomColor = "#" + ("000000" + color).slice(-6);
+    var colorRandom = p.color(randomColor);
 
     if (p.mouseIsPressed) {
-      p.ellipse(p.mouseX, p.mouseY, 3);
-      x = p.mouseX;
-      y = p.mouseY;
-      r = 0;
+      //xRand: -(blushSize/2)~blushSize/2までのランダムな整数
+      let xRand = Math.floor(p.random(blushSize)) - blushSize/2;
+      let yRand = Math.floor(p.random(blushSize)) - blushSize/2;
+      x = p.mouseX + xRand;
+      y = p.mouseY + yRand;
+
+      p.fill(colorRandom);
+      p.ellipse(x, y, 3);
     }
+
+    //p.rect(0, p.height-10, 0, p.height);
+    p.fill("#ffffff");
+    p.rect(0, p.height - 10, 100, p.height);
+    p.fill("#000000");
+    p.text("(" + p.mouseX + ", " + p.mouseY + ")", 0, p.height);
   };
 }
 
