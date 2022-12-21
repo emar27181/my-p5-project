211376930b799b1ea5df2f192f921fdcb82ca1fb
ema_ryu-home-desktop-pc <ee207008@meiji.ac.jp>
2022-12-21 23:29:53 +0900

    reqbodyでデータを受け取れるように(?)追記

--- server.js
+++ server.js
@@ -4,11 +4,18 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+
+app.use(express.static('public'));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
 app.get("/", (req, res) => {
     res.redirect("/home");
 });
 
-app.use(express.static('public'));
 app.get("/home", function(reqest, response){
     response.sendFile('/mnt/d/ryunosuke/my_WSL_dir/my-p5-project/src/index.html');
 });
